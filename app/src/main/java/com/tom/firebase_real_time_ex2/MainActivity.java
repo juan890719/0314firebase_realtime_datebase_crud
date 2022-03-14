58829bad4f67b744c28ceccba54ff563e5e0b49b
Juan <michelle.70623@gmail.com>
2022-03-14 11:28:15 +0800

    create, update, remove success!

---
+++ app/src/main/java/com/tom/firebase_real_time_ex2/MainActivity.java
@@ -0,0 +1,70 @@
+package com.tom.firebase_real_time_ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSubmit, btnUpdate, btnRemove;
+    private EditText etPosition, etName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.et_name);
+        etPosition = findViewById(R.id.et_position);
+        btnSubmit = findViewById(R.id.btn_submit);
+        btnUpdate = findViewById(R.id.btn_update);
+        btnRemove = findViewById(R.id.btn_remove);
+        DAOEmployee dao = new DAOEmployee();
+
+        // 新增
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Employee emp = new Employee(etName.getText().toString(), etPosition.getText().toString());
+                dao.add(emp).addOnSuccessListener(suc -> {
+                    Toast.makeText(MainActivity.this, "Record is inserted", Toast.LENGTH_SHORT).show();
+                }).addOnFailureListener(er -> {
+                    Toast.makeText(MainActivity.this, "" + er.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+
+        // 更新
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap<String, Object> hashMap = new HashMap<>();
+                hashMap.put("name", etName.getText().toString());
+                hashMap.put("position", etPosition.getText().toString());
+                dao.update("-My5S-mEdwRMHObKV96P", hashMap).addOnSuccessListener(suc -> {
+                    Toast.makeText(MainActivity.this, "Record is updated", Toast.LENGTH_SHORT).show();
+                }).addOnFailureListener(er -> {
+                    Toast.makeText(MainActivity.this, "" + er.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+
+        // 刪除
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dao.remove("-My5S-mEdwRMHObKV96P").addOnSuccessListener(suc -> {
+                    Toast.makeText(MainActivity.this, "Record is removed", Toast.LENGTH_SHORT).show();
+                }).addOnFailureListener(er -> {
+                    Toast.makeText(MainActivity.this, "" + er.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+    }
+}
